2a538496a4fcc978679982d0ee1735769e09261f
antoniojss1 <antoniojss1a@gmail.com>
2023-04-27 12:32:51 +0200

    Cartas
    He añadido las cartas para poder hacer pruebas con ella, los sprites que aparecen son de pruebas

---
+++ Assets/Scripts/card/CardDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public TMP_Text nameText;
+    public TMP_Text descriptionText;
+
+    public Image artworkImage;
+
+    void Start() 
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artworkImage;
+    }
+}
